4368381d302d4553ac51a450580d65a1617c602b
anasir514 <anasir514@gmail.com>
2021-01-19 21:52:26 +0000

    first commit

---
+++ main_app/first_app/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render, redirect
+from .models import Tutorial
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import logout, authenticate, login
+from django.contrib import messages
+ 
+def homepage(request):
+   return render(request = request,
+                 template_name='homepage.html',
+                 context = {"tutorials":Tutorial.objects.all})
+ 
+def register(request):
+   if request.method == "POST":
+       form = UserCreationForm(request.POST)
+       if form.is_valid():
+           user = form.save()
+           username = form.cleaned_data.get('username')
+           messages.success(request, f"New account created: {username}")
+           login(request, user)
+           return redirect("homepage")
+ 
+       else:
+           for msg in form.error_messages:
+               messages.error(request, f"{msg}: {form.error_messages[msg]}")
+ 
+           return render(request = request,
+                         template_name = "register.html",
+                         context={"form":form})
+ 
+   form = UserCreationForm
+   return render(request = request,
+                 template_name = "register.html",
+                 context={"form":form})
