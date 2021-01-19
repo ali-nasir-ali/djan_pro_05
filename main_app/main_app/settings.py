77b4aca1fe705cbd1353b666473a6ffb844e390f
anasir514 <anasir514@gmail.com>
2021-01-19 22:01:36 +0000

    tinymce

--- main_app/main_app/settings.py
+++ main_app/main_app/settings.py
@@ -36,6 +36,34 @@ DEBUG = os.environ['DJANGO_DEBUG']
 ALLOWED_HOSTS = [os.environ['DJANGO_ALLOWED_HOSTS']]
 
 
+TINYMCE_DEFAULT_CONFIG = {
+    'height': 360,
+    'width': 1120,
+    'cleanup_on_startup': True,
+    'custom_undo_redo_levels': 20,
+    'selector': 'textarea',
+    'theme': 'modern',
+    'plugins': '''
+            textcolor save link image media preview codesample contextmenu
+            table code lists fullscreen  insertdatetime  nonbreaking
+            contextmenu directionality searchreplace wordcount visualblocks
+            visualchars code fullscreen autolink lists  charmap print  hr
+            anchor pagebreak
+            ''',
+    'toolbar1': '''
+            fullscreen preview bold italic underline | fontselect,
+            fontsizeselect  | forecolor backcolor | alignleft alignright |
+            aligncenter alignjustify | indent outdent | bullist numlist table |
+            | link image media | codesample |
+            ''',
+    'toolbar2': '''
+            visualblocks visualchars |
+            charmap hr pagebreak nonbreaking anchor |  code |
+            ''',
+    'contextmenu': 'formats | link image',
+    'menubar': True,
+    'statusbar': True,
+    }
 # Application definition
 
 INSTALLED_APPS = [
@@ -48,6 +76,7 @@ INSTALLED_APPS = [
     # our apps
     'crispy_forms',
     'first_app',
+    'tinymce',
 
 ]
 
@@ -66,7 +95,7 @@ ROOT_URLCONF = 'main_app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
