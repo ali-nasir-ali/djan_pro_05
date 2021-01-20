3fae438ad852e8ab84e31d23305ba341c287fe89
anasir514 <anasir514@gmail.com>
2021-01-20 11:45:36 +0000

    by

--- main_app/first_app/views.py
+++ main_app/first_app/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render, redirect
-from .models import Tutorial
+from .models import Tutorial, TutorialCategory, TutorialSeries
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import logout, authenticate, login
 from django.contrib import messages
  
 def homepage(request):
-   return render(request = request,
-                 template_name='homepage.html',
-                 context = {"tutorials":Tutorial.objects.all})
- 
+    return render(request=request,
+                  template_name='main/categories.html',
+                  context={"categories": TutorialCategory.objects.all})
+                  
 def register(request):
    if request.method == "POST":
        form = UserCreationForm(request.POST)
@@ -34,3 +34,13 @@ def register(request):
 
 
 # we will ust the django internal login and logout they are easy to use
+def single_slug(request, single_slug):
+    categories = [c.category_slug for c in TutorialCategory.objects.all()]
+    if single_slug in categories:
+      return HttpResponse(f"{single_slug} is a category")
+
+    tutorials = [t.tutorial_slug for t in Tutorial.objects.all()]
+    if single_slug in tutorials:
+      return HttpResponse(f"{single_slug} is a Tutorial")
+
+    return HttpResponse(f"'{single_slug}' does not correspond to anything we know of!")
